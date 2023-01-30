f4202b2cb4757d19b5cee2ad4665da4b984541ca
Chris McDonnell <c.a.mcdonn@gmail.com>
2023-01-29 20:50:08 -0500

    Changed class to function

--- frontend/src/JoinGame/Home.tsx
+++ frontend/src/JoinGame/Home.tsx
@@ -1,27 +1,30 @@
-import React from "react";
-import { Link as Link } from "react-router-dom";
-type homeProps = {
-    sendName: Function;
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+type HomeProps = {
+    sendName: (name: string) => void;
 }
 
-class Home  extends React.Component <homeProps>{
-    state = { username: "" }
-    handleChange = (e: any) => {
-            this.props.sendName(e.target.value);
+export default function Home(props: HomeProps) {
+    const [username, _setUsername] = useState("");
+    const handleChange = (e: any) => {
+        props.sendName(e.target.value);
     }
-    render () {
-        return (
-            <div className = 'background flex'>
-                <button className = 'joinButton center'><Link to='/lobby' className = 'among-us-join'>Join Game</Link></button>
-                <div className = 'flex'>
-                    <label className = 'user-label' htmlFor="name">Username:</label>
-                    <input onChange={this.handleChange} className = 'user-input' 
-                    type="text" defaultValue = {this.state.username}
-                     required minLength= {1} maxLength={10} size={12}></input>
-                </div>
+    const navigate = useNavigate();
+    const joinGame = (_e: any) => {
+        navigate("/lobby");
+    }
+    return (
+        <div className='background flex'>
+            <button className='joinButton center among-us-join' onClick={joinGame}>
+                Join Game
+            </button>
+            <div className='flex'>
+                <label className='user-label' htmlFor="name">Username:</label>
+                <input onChange={handleChange} className='user-input'
+                    type="text" defaultValue={username}
+                    required minLength={1} maxLength={10} size={12}></input>
             </div>
-        );
-        }   
+        </div>
+    );
 }
 
-export default Home;
