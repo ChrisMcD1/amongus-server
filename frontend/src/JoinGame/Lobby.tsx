01f2dd6352165e2cf55462c1a98949947e806931
Chris McDonnell <c.a.mcdonn@gmail.com>
2023-01-29 21:37:37 -0500

    Changed lobby to functional component

--- frontend/src/JoinGame/Lobby.tsx
+++ frontend/src/JoinGame/Lobby.tsx
@@ -1,51 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactComponent as White } from './Whitetest.svg';
 import { BlockPicker, ColorChangeHandler, ColorResult } from 'react-color';
 //@ts-ignore
 import { LightenDarkenColor } from 'lighten-darken-color';
 import start from "./start.png"
 
-type lobbyProps = {username: string}
-type lobbyStates = {
-    background: string,
-    check: boolean,
-}
+type LobbyProps = { username: string }
 
-class Lobby extends React.Component<lobbyProps, lobbyStates> {
+export default function Lobby(props: LobbyProps) {
+    const [background, setBackground] = useState("#000000");
+    const [check, setCheck] = useState(false);
 
-    constructor(props: lobbyProps) {
-        super(props);
-        this.state = {
-            background: "#000000",
-            check: false,
-        }
-    }
-
-
-
-    handleChange = (color: ColorResult) => {
-        this.setState(prevState => ({...prevState, background: color.hex, }));
+    const handleChange = (color: ColorResult) => {
+        setBackground(color.hex);
         document.documentElement.style.setProperty('--base-color', color.hex);
         document.documentElement.style.setProperty('--shadow-color', LightenDarkenColor(color.hex, -35));
     }
-    myfunction() {
+
+    const myFunction = () => {
         console.log("CLICKED");
     }
-    render() {
-        return (
-            <div className='lobby-background'>
-                <div className='player-box center'>
-                    <h3 className = 'player-text'>{this.props.username}</h3>
-                    <White className="player" onClick={() => this.setState(prevState => ({...prevState, check: !prevState.check,}))} />
-                    <button style={{ display: this.state.check ? "none" : "initial" }} className = "start-button"><img src= {start} onClick={this.myfunction} /></button>
-                </div>
-                <div style={{ display: this.state.check ? "initial" : "none" }} className={'colorPick center'}>
-                    <BlockPicker color = {this.state.background} onChange={this.handleChange} />
-                </div>
+
+    return (
+        <div className='lobby-background'>
+            <div className='player-box center'>
+                <h3 className='player-text'>{props.username}</h3>
+                <White className="player" onClick={() => setCheck(!check)} />
+                <button style={{ display: check ? "none" : "initial" }}
+                    className="start-button">
+                    <img src={start} onClick={myFunction} />
+                </button>
             </div>
-        );
-    };
+            <div style={{ display: check ? "initial" : "none" }} className={'colorPick center'}>
+                <BlockPicker color={background} onChange={handleChange} />
+            </div>
+        </div>
+    );
 }
-
-
-export default Lobby;
