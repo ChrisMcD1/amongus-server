0d8ca692d7c16ce2543c78b361820ca212cf2f45
Chris McDonnell <c.a.mcdonn@gmail.com>
2023-02-26 20:54:46 -0500

    Fixed some formatting

--- frontend/src/InGame/PlayerTile.tsx
+++ frontend/src/InGame/PlayerTile.tsx
@@ -1,15 +1,15 @@
-import { Player } from "./StatusOverview";
+import { Player } from "../state/playersSlice"
 
 export default function PlayerTile(props: Player) {
-  console.log(props);
-  return (
-    <div className="m-1 flex flex-col items-center rounded-xl border-2 border-solid border-white p-2">
-      <div
-        style={{ backgroundColor: props.color }}
-        className="mx-auto h-20 w-20 rounded-full"
-      ></div>
-      <div>{props.name}</div>
-      <div>{props.alive ? "I'm alive" : "I'm dead"}</div>
-    </div>
-  );
+    console.log(props);
+    return (
+        <div className="m-1 flex flex-col items-center rounded-xl border-2 border-solid border-white p-2">
+            <div
+                style={{ backgroundColor: props.color }}
+                className="mx-auto h-20 w-20 rounded-full"
+            ></div>
+            <div>{props.username}</div>
+            <div>{props.alive ? "I'm alive" : "I'm dead"}</div>
+        </div>
+    );
 }
