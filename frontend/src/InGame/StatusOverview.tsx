29208d54d885e4a1c57c5cccea89e08da01c5785
Chris McDonnell <c.a.mcdonn@gmail.com>
2023-03-08 22:03:50 -0500

    fixed width on status overview

--- frontend/src/InGame/StatusOverview.tsx
+++ frontend/src/InGame/StatusOverview.tsx
@@ -11,7 +11,7 @@ function StatusOverview(props: StatusOverviewProps) {
     const otherPlayers = useAppSelector(selectOtherPlayers);
     const [selectedPlayerID, setSelectedPlayerID] = useState<string | undefined>(undefined);
     return (
-        <div className="flex h-screen flex-col place-content-center justify-center p-5">
+        <div className="flex h-screen w-screen flex-col place-content-center justify-center p-5">
             <div>
                 <div className="justify-left flex flex-wrap place-content-center">
                     {otherPlayers.map((player) => {
