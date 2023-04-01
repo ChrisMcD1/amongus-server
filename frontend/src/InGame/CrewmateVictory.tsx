d9b193ecea5ae0b5778db4dec3c50c61bdf69176
Chris McDonnell <c.a.mcdonn@gmail.com>
2023-04-01 00:54:48 -0400

    Made victory text white

--- frontend/src/InGame/CrewmateVictory.tsx
+++ frontend/src/InGame/CrewmateVictory.tsx
@@ -1,5 +1,5 @@
 export default function CrewmateVictory() {
     return (<div className="bg-green-800 w-screen h-screen grid">
-        <h1 className="text-3xl mx-auto my-auto">Crewmates Win!</h1>
+        <h1 className="text-white text-3xl mx-auto my-auto">Crewmates Win!</h1>
     </div>)
 }
--- frontend/src/InGame/ImposterVictory.tsx
+++ frontend/src/InGame/ImposterVictory.tsx
@@ -1,6 +1,6 @@
 
 export default function ImposterVictory() {
     return (<div className="bg-red-800 w-screen h-screen grid">
-        <h1 className="text-3xl mx-auto my-auto">Imposters Win!</h1>
+        <h1 className="text-white text-3xl mx-auto my-auto">Imposters Win!</h1>
     </div>)
 }
